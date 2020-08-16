d9595271b94b9b17e4e9bac26626c4beec45fee5
mani <kr630601@dal.ca>
2020-08-15 18:32:29 -0700

    rough form design

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+// import "popper.js/dist/popper.min.js";
+import "jquery/dist/jquery.slim.min.js";
+import "jquery/dist/jquery.min.js";
+import "./App.scss";
+import AddForm from "./components/AddForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={AddForm} />
+      </Switch>
+    </>
   );
 }
 
